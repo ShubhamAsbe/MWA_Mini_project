af5bde8e30d4de5fcf8ac962ab9e358fd503c2c8
Shubham Asbe <76616652+ShubhamAsbe@users.noreply.github.com>
2023-06-06 10:19:48 +0530

    MWA Mini project

---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,40 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>R</strong><span class = "small footerSubtext">ajashri</span>
+                        <strong>S</strong><span class = "small footerSubtext">hahu</span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngeering</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+            
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Pimple Nilakh, Panchashil Nagar<br>
+                    Pune 411027<br>
+                    Shubham Asbe,<br>
+                    Anuradha Partudkar,<br>
+                    Avinash Padale,<br>
+                    Avinash Andhale,<br>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
